8cdfd104457cb329cfafa27923d660bde14288f0
Caitlyn Hue <cdhue@Caitlyns-MacBook-Air.local>
2025-10-01 23:22:56 -0700

    completed program

--- Assignment 2/decision_tree_2.py
+++ Assignment 2/decision_tree_2.py
@@ -4,7 +4,7 @@
 # SPECIFICATION: Read three training files to train, test, and output the performance
 #                of the 3 models created on the test set.
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 2
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: YOU ARE ALLOWED TO USE ANY PYTHON LIBRARY TO COMPLETE THIS PROGRAM
@@ -21,50 +21,78 @@ df_test = pd.read_csv('contact_lens_test.csv')
 for _, row in df_test.iterrows():
     dbTest.append(row.tolist())
 
-for ds in dataSets:
+age_map = {"Young": 1, "Prepresbyopic": 2, "Presbyopic": 3}
+spectacle_map = {"Myope": 1, "Hypermetrope": 2}
+astigmatism_map = {"Yes": 1, "No": 2}
+tear_map = {"Reduced": 1, "Normal": 2}
+class_map = {"Yes": 1, "No": 2}
 
-    dbTraining = []
+for ds in dataSets:
     X = []
     Y = []
 
     #Reading the training data in a csv file using pandas
     # --> add your Python code here
-    #dbTraining =
+    dbTraining = []
+    df_train = pd.read_csv(ds)
+
+    for _, row in df_train.iterrows():
+        dbTraining.append(row.tolist())
 
     #Transform the original categorical training features to numbers and add to the 4D array X.
     #For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
-    #--> add your Python code here
-
+    #--> add your Python code here  
     #Transform the original categorical training classes to numbers and add to the vector Y.
     #For instance Yes = 1 and No = 2, Y = [1, 1, 2, 2, ...]
     #--> add your Python code here
+    for row in dbTraining:
+        X.append([age_map[row[0]],
+                 spectacle_map[row[1]],
+                 astigmatism_map[row[2]],
+                 tear_map[row[3]]])
+        Y.append(class_map[row[4]])
 
     #Loop your training and test tasks 10 times here
-    #for i in range (10):
+    total_accuracy = 0
+    for i in range(10):
+        # fitting the decision tree to the data using entropy as your impurity measure and maximum depth = 5
+        # --> add your Python code here
+        clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)
+        clf = clf.fit(X,Y)
 
-       # fitting the decision tree to the data using entropy as your impurity measure and maximum depth = 5
-       # --> addd your Python code here
-       # clf =
-
-       #Read the test data and add this data to dbTest
-       #--> add your Python code here
-
-     #  for data in dbTest:
+        #Read the test data and add this data to dbTest
+        #--> add your Python code here
+        correct = 0
+        for data in dbTest:
            #Transform the features of the test instances to numbers following the same strategy done during training,
            #and then use the decision tree to make the class prediction. For instance: class_predicted = clf.predict([[3, 1, 2, 1]])[0]
            #where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
            #--> add your Python code here
+           test_instance = [
+                            age_map[data[0]],
+                            spectacle_map[data[1]],
+                            astigmatism_map[data[2]],
+                            tear_map[data[3]]
+           ]
+           class_predicted = clf.predict([test_instance])[0]
+           true_class = class_map[data[4]]
 
            #Compare the prediction with the true label (located at data[4]) of the test instance to start calculating the accuracy.
            #--> add your Python code here
+           if class_predicted == true_class:
+               correct += 1
+        
+        accuracy = correct / len(dbTest)
+        total_accuracy += accuracy
 
     #Find the average of this model during the 10 runs (training and test set)
     #--> add your Python code here
+        total_accuracy /= 10
 
     #Print the average accuracy of this model during the 10 runs (training and test set).
     #Your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
     #--> add your Python code here
-
+    print(f"final accuracy when training on {ds}: {total_accuracy:.3f}")
 
 
 
